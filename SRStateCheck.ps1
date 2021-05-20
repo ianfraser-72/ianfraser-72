554663a4798800f5cf6259adea316c206268eeda
ianfraser-72 <79473120+ianfraser-72@users.noreply.github.com>
2021-05-20 13:44:57 +0100

    Update SRStateCheck.ps1

--- SRStateCheck.ps1
+++ SRStateCheck.ps1
@@ -1,6 +1,6 @@
 state = get-windowsfeature storage*
-$state .name
-if ($state .name -match "Storage-Replica")
+$state.name
+if ($state.name -match "Storage-Replica")
 {
 write-host "Replica Installed"
 Exit 0
