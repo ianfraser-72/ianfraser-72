71696581a31660e324ade109244d0269add781de
ianfraser-72 <79473120+ianfraser-72@users.noreply.github.com>
2021-05-20 15:01:59 +0100

    Update TestSR.ps1

--- TestSR.ps1
+++ TestSR.ps1
@@ -1,4 +1,4 @@
 # Test Storage Replicate connectivity
 $source = $args[0]
 $destination = $args[1]
-Test-SRTopology "-SourceComputerName $source" "-SourceVolumeName E:" "-SourceLogVolumeName G:" "-DestinationComputerName $destination" "-DestinationVolumeName E:" "-DestinationLogVolumeName G:" "-DurationInMinutes 5" "-ResultPath c:\temp"
+Test-SRTopology -SourceComputerName $source -SourceVolumeName E: -SourceLogVolumeName G: -DestinationComputerName $destination -DestinationVolumeName E: -DestinationLogVolumeName G: -DurationInMinutes 5 -ResultPath c:\temp
