b8378584c654b0bb11e9c9c22ba273a6b422c7bb
ianfraser-72 <79473120+ianfraser-72@users.noreply.github.com>
2021-05-20 14:58:59 +0100

    Update TestSR.ps1

--- TestSR.ps1
+++ TestSR.ps1
@@ -1,3 +1,3 @@
 $source = $args[0]
 $destination = $args[1]
-Test-SRTopology "-SourceComputerName $source" "-SourceVolumeName E:" "-SourceLogVolumeName G:" "-DestinationComputerName $destination" "-DestinationVolumeName E:" "-DestinationLogVolumeName G:" "-DurationInMinutes 5" "-ResultPath c:\temp" "–verbose"
+Test-SRTopology "-SourceComputerName $source" "-SourceVolumeName E:" "-SourceLogVolumeName G:" "-DestinationComputerName $destination" "-DestinationVolumeName E:" "-DestinationLogVolumeName G:" "-DurationInMinutes 5" "-ResultPath c:\temp"
