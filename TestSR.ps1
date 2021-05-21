fca7826edf3ac11cc9ea1f15543663220d823b14
ianfraser-72 <79473120+ianfraser-72@users.noreply.github.com>
2021-05-21 11:41:55 +0100

    Update TestSR.ps1

--- TestSR.ps1
+++ TestSR.ps1
@@ -7,4 +7,4 @@ $destserver = $args[3]
 $destdatavol = $args[4]
 $destlogvol = $args[5]
 
-Test-SRTopology -SourceComputerName $sourceserver -SourceVolumeName $sourcedatavol":" -SourceLogVolumeName $sourcelogvol":" -DestinationComputerName $destserver -DestinationVolumeName $destdatavol":" -DestinationLogVolumeName $destlogvol":" -DurationInMinutes 5 -ResultPath c:\temp
+Test-SRTopology -SourceComputerName $sourceserver -SourceVolumeName $sourcedatavol":" -SourceLogVolumeName $sourcelogvol":" -DestinationComputerName $destserver -DestinationVolumeName $destdatavol":" -DestinationLogVolumeName $destlogvol":" -DurationInMinutes 5 -ResultPath c:\temp -verbose
