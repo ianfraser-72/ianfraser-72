fd2baad690680e76ae125f97c3aa69ceda3b8109
ianfraser-72 <79473120+ianfraser-72@users.noreply.github.com>
2021-05-24 16:02:09 +0100

    Update StorageReplicaBuild.ps1

--- StorageReplicaBuild.ps1
+++ StorageReplicaBuild.ps1
@@ -1,4 +1,5 @@
-# Build Storage Replicas
+# Build Storage Replica servers via Ansible
+# Passes TaskType vriable to the script for the Test, Build and Remove phases, and diskcheck and statecheck.
 
 param
 (
@@ -16,22 +17,37 @@ param
 
 If ($task -eq "Build")
 {
-New-SRPartnership -SourceComputerName $sourceserver -SourceRGName $sourcerg -SourceVolumeName $sourcedatavol":" -SourceLogVolumeName $sourcelogvol":" -DestinationComputerName $destserver -DestinationRGName $destrg -DestinationVolumeName $destdatavol":" -DestinationLogVolumeName $destlogvol":"
+  New-SRPartnership -SourceComputerName $sourceserver -SourceRGName $sourcerg -SourceVolumeName $sourcedatavol":" -SourceLogVolumeName $sourcelogvol":" -DestinationComputerName $destserver -DestinationRGName $destrg -DestinationVolumeName $destdatavol":" -DestinationLogVolumeName $destlogvol":"
 }
 
 if ($task -eq "Test")
 {
-Test-SRTopology -SourceComputerName $sourceserver -SourceVolumeName $sourcedatavol":" -SourceLogVolumeName $sourcelogvol":" -DestinationComputerName $destserver -DestinationVolumeName $destdatavol":" -DestinationLogVolumeName $destlogvol":" -DurationInMinutes 5 -ResultPath c:\temp -verbose
+  Test-SRTopology -SourceComputerName $sourceserver -SourceVolumeName $sourcedatavol":" -SourceLogVolumeName $sourcelogvol":" -DestinationComputerName $destserver -DestinationVolumeName $destdatavol":" -DestinationLogVolumeName $destlogvol":" -DurationInMinutes 5 -ResultPath c:\temp -verbose
 }
 
 if ($task -eq "Remove")
 {
-Remove-SRPartnership -SourceComputerName $sourceserver -SourceRGName $sourcerg -DestinationComputerName $destserver -DestinationRGName $destrg
+  Remove-SRPartnership -SourceComputerName $sourceserver -SourceRGName $sourcerg -DestinationComputerName $destserver -DestinationRGName $destrg
 }
 
 if ($task -eq "diskcheck")
 {
-$destserver = $args[0]
-$serverdatavol = $args[1]
-net use t: \\$destserver\$serverdatavol"`$"
+  $destserver = $args[0]
+  $serverdatavol = $args[1]
+  net use t: \\$destserver\$serverdatavol"`$"
+}
+
+if ($task -eq "statecheck")
+{
+   $state = get-windowsfeature stor*
+   if ($state.name -match "Storage-Replica")
+   {
+   write-host "Storage Replica Installed"
+   Exit 0
+   }
+else
+   {
+   write-host "Storage Replica not Installed"
+   Exit 2
+   }
 }
