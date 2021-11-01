2acd75feb5b057b456b7aaccbfa2c59abe450dff
ianfraser-72 <79473120+ianfraser-72@users.noreply.github.com>
2021-11-01 22:09:17 +0000

    Update StorageReplicaBuild.ps1

--- StorageReplicaBuild.ps1
+++ StorageReplicaBuild.ps1
@@ -203,6 +203,9 @@ if (($task -eq "failover") -and ($cnamefailover -ne ""))
     
   try
   {  
+    $runstring = "SETSPN -d host/$sourceserver2"
+    $run = invoke-expression $runstring
+    Start-Sleep -s 10
     $runstring = "SETSPN -a host/alias $sourceserver2"
     $run = invoke-expression $runstring
     $run
